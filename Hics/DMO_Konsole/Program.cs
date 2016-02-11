751a909e2a8c0d3a5822df5c6f9dcbff05703f94
christa kornfeld <kornfrau@gmail.com>
2016-02-11 14:33:09 +0100

    seas-ausgabe

--- Hics/DMO_Konsole/Program.cs
+++ Hics/DMO_Konsole/Program.cs
@@ -11,6 +11,7 @@ namespace DMO_Konsole
         static void Main(string[] args)
         {
             Console.WriteLine("Hallo Welt");
+            Console.WriteLine("seas");
 
             Console.ReadLine();
         }
