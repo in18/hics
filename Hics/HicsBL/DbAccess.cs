38d265dd1d63ee5f0687698e51f1468dc2b51571
Bernhard Wolf <bernhard.wolf.74@gmail.com>
2016-02-19 16:01:47 +0100

    Kom. eingefügt

--- Hics/HicsBL/DbAccess.cs
+++ Hics/HicsBL/DbAccess.cs
@@ -17,6 +17,7 @@ namespace HicsBL
         /// <param name="lampname"></param>
         static void addLamp(string username, string password, string lampadress, string lampname)
         {
+            //Übergebenes Passwort hashen und in Var pwhas speichern für Übergabe an DB
             string pwhash = HelperClass.GetHash(password);
 
         }
@@ -27,6 +28,7 @@ namespace HicsBL
         /// <param name="password"></param>
          static void addUser(string username, string password)
         {
+            //Übergebenes Passwort hashen und in Var pwhas speichern für Übergabe an DB
             string pwhash = HelperClass.GetHash(password);
         }
 
