4a82d01138d904e8c3d540e2863efbbce9a5c297
Leopold Hehenberger <l.hehenberger@gmx.at>
2016-02-22 14:20:05 +0100

    Hehenberger: LoginController + LoginView erstellt

---
+++ Hics/HicsMVC/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HicsMVC.Controllers
+{
+    public class LoginController : Controller
+    {        
+        /// <summary>
+        /// GET ActionResult für Login View
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
