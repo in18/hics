ce480bcb468521a6f23e47147bbb26612df451e3
Sebastian Pfeifer <nagrond@hotmail.com>
2016-03-01 15:43:05 +0100

    Admin LampSetupModel und Controller

--- Hics/HicsMVC/Controllers/LampSetupController.cs
+++ Hics/HicsMVC/Controllers/LampSetupController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HicsBL;
+using HicsMVC.Models;
+using System.Diagnostics;
 
 namespace HicsMVC.Controllers
 {
@@ -11,7 +14,21 @@ namespace HicsMVC.Controllers
         // GET: LampSetup
         public ActionResult Index()
         {
+            //LampSetupModel lampSetupModel = new LampSetupModel();
+            
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Index(LampSetupModel lsm)
+        {
+                        
+            Debug.WriteLine($"ID: {lsm.id}\nDesc: {lsm.description}");
+            
+            
+
+            return RedirectToAction("index");
+        }
+
     }
 }
