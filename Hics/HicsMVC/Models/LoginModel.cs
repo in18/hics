5005cad6da7c66d2d0457d5db30f8afbf48d4b8b
Leopold Hehenberger <l.hehenberger@gmx.at>
2016-03-14 16:17:36 +0100

    UserChangePassword, LoginLogin u. AdminResetPasswort - Validierung erweitert und auf Razor geändert.

--- Hics/HicsMVC/Models/LoginModel.cs
+++ Hics/HicsMVC/Models/LoginModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +9,12 @@ namespace HicsMVC.Models
     public class LoginModel
     {
         //Felder & Eigenschaften
+
+        // [] = Attribute - stehen direkt darüber
+        [Required(ErrorMessage = "required field", AllowEmptyStrings = false)]
         public string Username { get; set; }
-        public string Password { get; set; }
-        
+
+        [Required(ErrorMessage = "required field", AllowEmptyStrings = false)]
+        public string Password { get; set; }        
     }    
 }
