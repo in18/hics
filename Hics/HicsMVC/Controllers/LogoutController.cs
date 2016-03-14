f0cfaf597b538e4f612a71d82302fa297b682d29
Leopold Hehenberger <l.hehenberger@gmx.at>
2016-03-14 16:46:01 +0100

    Model und Controller "Logout" hinzugefügt

---
+++ Hics/HicsMVC/Controllers/LogoutController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HicsMVC.Controllers
+{
+    public class LogoutController : Controller
+    {        
+        /// <summary>
+        /// Methode: [HttpGet]
+        /// Methode ruft Logout-View auf
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Logout()
+        {
+            return View();
+
+            //View noch hinzufügen!
+        }
+    }
+}
---
+++ Hics/HicsMVC/Models/LogoutModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HicsMVC.Models
+{
+    public class LogoutModel
+    {
+        //Felder & Eigenschaften
+
+        // [] = Attribute - stehen direkt darüber
+        //[Required(ErrorMessage = "required field", AllowEmptyStrings = false)]
+        //public string "#" { get; set; }        
+    }
+}
