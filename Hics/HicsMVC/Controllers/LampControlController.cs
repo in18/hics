25c7a8ca768236f64d822b8e8eb0ab9a7958121f
Andrea Kocerke <andrea.kocerke@outlook.com>
2016-03-21 16:36:41 +0100

    AdminLampcontroll

--- Hics/HicsMVC/Controllers/LampControlController.cs
+++ Hics/HicsMVC/Controllers/LampControlController.cs
@@ -16,7 +16,10 @@ namespace HicsMVC.Controllers
         [HttpGet]
         public ActionResult Index()
         {
-            List<LampControl> lamps = DbHelper.DbHelperClass.getLamps();
+            List<HicsBL.fn_show_lamp_control_Result> lamps;
+
+            lamps = HicsBL.DbAccess.GetAllLampsStatus("admin", "123user!").ToList();
+                           
             return View(lamps);
         }
 
@@ -36,13 +39,19 @@ namespace HicsMVC.Controllers
         /// <param name="id"></param>
         /// <returns></returns>
         [HttpGet]
-        public ActionResult Edit(int id)
+        public ActionResult Edit(int? id)
         {
+            List<HicsBL.fn_show_lamp_control_Result> lamp;
+            lamp = HicsBL.DbAccess.GetAllLampsStatus("admin", "123user!").ToList();
+            
+             HicsBL.fn_show_lamp_control_Result erg = lamp.Where(x => x.lamp_id == id).FirstOrDefault();
             //List<LampControl> lamps = DbHelper.DbHelperClass.getLamps();
-            List<HicsBL.fn_show_lamps_Result> lamps = HicsBL.DbAccess.GetAllLamps("admin","123user!");
+            // List<HicsBL.fn_show_lamps_Result> lamps = HicsBL.DbAccess.GetAllLamps("admin","123user!");
             //LampControl lc = (from a in lamps where a.id == id select a).FirstOrDefault<LampControl>();
-            lamps = ((lamps.Where(x => x.id == id)).ToList());
-            return View(lamps);
+
+            
+
+            return View(erg);
         }
 
 
@@ -53,13 +62,13 @@ namespace HicsMVC.Controllers
         /// <param name="id"></param>
         /// <param name="groupname"></param>
         /// <param name="lampname"></param>
-        /// <param name="onOff"></param>
-        /// <param name="dimmer"></param>
+        /// <param name="status"></param>
+        /// <param name="brigthness"></param>
         /// <returns>übernimmt die Daten vom View und speichert diese auf dem Server</returns>
         [HttpPost]
-        public ActionResult Edit (LampControl l)
+        public ActionResult Edit (HicsBL.fn_show_lamp_control_Result l)
         {
-            HicsBL.DbAccess.dimLamp("Hugo", "lmaa", l.id, l.dimmer);
+            HicsBL.DbAccess.dimLamp("admin", "123user!", (int)l.lamp_id, (byte)l.brightness);
 
             return View();
 
