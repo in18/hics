b920d7045d5507390aa0042301e4ca2f41e73630
Sebastian Pfeifer <nagrond@hotmail.com>
2016-04-04 16:26:46 +0200

    UserLampControlControll Änderungen

--- Hics/HicsMVC/Models/UserLampControlModel.cs
+++ Hics/HicsMVC/Models/UserLampControlModel.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using HicsBL;
 
 namespace HicsMVC.Models
 {
-    public class UserLampControl
+    public class UserLampControlModel
     {
         public Nullable<int> id { get; set; }
         public string name { get; set; }
         public string address { get; set; }
         public Nullable<bool> status { get; set; }
         public Nullable<byte> bright { get; set; }
+
+        public List<fn_show_lamp_control_Result> lamplist { get; set; }
     }
 }
