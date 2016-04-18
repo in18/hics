c3e994b6c31e323d3c37d07548e133d86141fe98
bernhard.wolf <bernhard.wolf.74@gmail.com>
2016-04-18 10:12:57 +0200

    com

--- Hics/HicsBL/Testing.cs
+++ Hics/HicsBL/Testing.cs
@@ -62,15 +62,15 @@ namespace HicsBL
             //    Console.WriteLine($"Datum:{i.date} Lampe:{i.lamp_name} Status:{i.status} User:{i.user_name}");
             //}
             //DbAccess.addUser("admin", "123user!", "maxi", "123user!", false);
-            using (itin18_aktEntities cont = new itin18_aktEntities())
-            {
-                List<fn_show_lamp_control_Result> l = cont.fn_show_lamp_control("admin", ha).ToList();
+            //using (itin18_aktEntities cont = new itin18_aktEntities())
+            //{
+            //    List<fn_show_lamp_control_Result> la = cont.fn_show_lamp_control("admin", ha).ToList();
             
-                foreach (var i in l)
-                {
-                    Console.WriteLine($"{i.lampname}|{i.groupname}");
-                }
-            }
+            //    foreach (var i in la)
+            //    {
+            //        Console.WriteLine($"{i.lampname}|{i.groupname}");
+            //    }
+            //}
             //using (itin18_aktEntities cont = new itin18_aktEntities())
             //{
             //    List<fn_show_lampgroups_Result> l = cont.fn_show_lampgroups("admin", ha).ToList();
@@ -79,13 +79,13 @@ namespace HicsBL
             //        Console.WriteLine($"{i.id}|{i.roomgroupname}");
             //    }
             //}
-            DbAccess.addLampToGroup("admin","123user!", 16, 1);
-            List < fn_show_users_Result > l = new List<fn_show_users_Result>();
-            l= DbAccess.GetAllUser("admin", "123user!");
-            foreach (var item in l)
-            {
-                Console.WriteLine(item.name);
-            }
+            //DbAccess.addLampToGroup("admin","123user!", 16, 1);
+            //List < fn_show_users_Result > l = new List<fn_show_users_Result>();
+            //l= DbAccess.GetAllUser("admin", "123user!");
+            //foreach (var item in l)
+            //{
+            //    Console.WriteLine(item.name);
+            //}
             
             Console.ReadKey();
 
