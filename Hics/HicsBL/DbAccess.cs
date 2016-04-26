78c3fb78a4fc460774677cc1ce3621c84479f1e0
christa kornfeld <kornfrau@gmail.com>
2016-04-26 07:56:30 +0200

    doppelte einträge geloescht

--- Hics/HicsBL/DbAccess.cs
+++ Hics/HicsBL/DbAccess.cs
@@ -589,24 +589,6 @@ namespace HicsBL
         }
         #endregion
 
-        #region PSP 7.4 editLampGroup(string username, string password, int groupId)
-        /// <summary>
-        /// PSP 7.4
-        /// Lampengruppen editieren
-        /// </summary>
-        /// <param name="username"></param>
-        /// <param name="password"></param>
-        /// <param name="groupId"></param>
-        /// <returns></returns>
-        public static bool editLampGroup(string username, string password, int groupId)
-        {
-            bool success = false;
-            //Übergebenes Passwort hashen und in Var pwhash speichern für Übergabe an DB
-            Byte[] pwhash = HelperClass.GetHash(password);
-            return success;
-
-        }
-        #endregion
 
         #region PSP  7.4 editLampGroup (string username, string password, int groupId)
         /// <summary>
